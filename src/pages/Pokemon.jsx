ed8339b92aee036728cf3c5a3143a08c3f2eb595
Edgar Méndez <mdzredgar@gmail.com>
2023-02-19 14:00:56 -0600

    Terminado Pokemon.jsx

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Pokemon = () => {
+  const [pokemon, setPokemon] = useState()
+
+  const {id} = useParams()
+
+  useEffect(() => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${id}/`
+    axios
+    .get(URL)
+    .then((res) => setPokemon(res.data))
+    .catch((err) => console.log(err))
+  }, [])
+
   return (
-    <div>Pokemon</div>
-  )
+    <main>
+      <section>
+        {/* Parte superior */}
+        <section>
+          <div>
+            <img
+              src={pokemon?.sprites.other["official-artwork"].front_default}
+              alt=""
+            />
+          </div>
+        </section>
+      </section>
+
+      {/* Body */}
+      <section>
+        <h2># {pokemon?.id}</h2>
+        <h2>{pokemon?.name}</h2>
+        <div>
+          <div>
+            <h5>Weight</h5>
+            <h4>{pokemon?.weight}</h4>
+          </div>
+          <div>
+            <h5>Height</h5>
+            <h4>{pokemon?.height}</h4>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Type</h3>
+            <div>
+              {pokemon?.types.map((type) => (
+                <div key={type.type.name}>
+                  <span>{type.type.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h3>Abilities</h3>
+            <div>
+              {pokemon?.abilities.map((ability) => (
+                <div key={ability.ability.name}>
+                  <span>{ability.ability.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Stats */}
+        <section>
+          <h2>Stats</h2>
+          <section>
+            {pokemon?.stats.map((stat) => (
+              <article key={stat.stat.name}>
+                <div>
+                  <h4>{stat.stat.name}</h4>
+                  <h5>{stat.base_stat}/150</h5>
+                </div>
+                <div>
+                  <div>
+                    <div></div>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </section>
+        </section>
+      </section>
+    </main>
+  );
 }
 
 export default Pokemon
