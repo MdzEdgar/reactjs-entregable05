ebd94bb25bec5e70c23a8499a430eb6049026040
Edgar Méndez <mdzredgar@gmail.com>
2023-02-19 16:30:14 -0600

    Mejora de codigo(Identación)

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
+import Header from './layout/Header'
 
 const ProtectedRoutes = () => {
 
   const nameTrainer = useSelector(store => store.nameTrainer)
   
   if(nameTrainer){
-    return <Outlet />  
+    return (
+      <>
+        <Header />
+        <Outlet />
+      </>
+    ); 
   }else {
     return <Navigate to="/" />
   }
