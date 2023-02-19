9ea34c88e3a5495505fb34ff075373a329ad6e20
Edgar Méndez <mdzredgar@gmail.com>
2023-02-19 16:31:24 -0600

    Correción de indentación

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import store from './store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
