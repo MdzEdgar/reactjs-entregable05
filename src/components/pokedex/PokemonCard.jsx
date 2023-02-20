674b38f337d32af163ac43b395b0b31c69446fc3
Edgar Méndez <mdzredgar@gmail.com>
2023-02-19 22:36:31 -0600

    PokemonCard finalizada.

--- src/components/pokedex/PokemonCard.jsx
+++ src/components/pokedex/PokemonCard.jsx
@@ -1,9 +1,16 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import "./styles/PokemonCard.css"
 
 const PokemonCard = ({pokemonUrl}) => {
-
   const [pokemon, setPokemon] = useState()
+
+  const navigate = useNavigate()
+
+  const handleClickPokemon = () => {
+    navigate(`/pokedex/${pokemon.id}`)
+  }
 
   useEffect(() => {
     axios.get(pokemonUrl)
@@ -12,26 +19,25 @@ const PokemonCard = ({pokemonUrl}) => {
   }, [])
 
   return (
-    <article >
-      <section></section>
-      <section>
-        <div>
+    <article className={`pokemonCard border-${pokemon?.types[0].type.name}`} onClick={handleClickPokemon}>
+      <section className={`pokemonCard__header bg-lg-${pokemon?.types[0].type.name}`}></section>
+      <section className='pokemonCard__body'>
+        <div className='pokemonCard__img'>
           <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
         </div>
-        <h3>{pokemon?.name}</h3>
-        <h4>{pokemon?.types[0].type.name} {pokemon?.types[1] && `/${pokemon?.types[1].name}` }</h4>
-        <h6>Tipo</h6>
-        <hr />
-        <section>
+        <h3 className='pokemonCard__name'>{pokemon?.name}</h3>
+        <h4 className='pokemonCard__types'>{pokemon?.types[0].type.name} {pokemon?.types[1] && `/${pokemon?.types[1].type.name}` }</h4>
+        <h6 className='pokemonCard__type-subtitle'>Tipo</h6>
+        <hr className='pokemonCard__line' />
+        <section className='pokemonCard__stats'>
           {
             pokemon?.stats.map(stat => (
-              <div key={stat.stat.url} >
-                <h5>{stat.stat.name}</h5>
-                <h5>{stat.base_stat}</h5>
+              <div className='pokemonCard__stat' key={stat.stat.url} >
+                <h5 className='pokemonCard__stat-name'>{stat.stat.name}</h5>
+                <h5 className='pokemonCard__stat-value' >{stat.base_stat}</h5>
               </div>
             ))
           }
-          
         </section>
       </section>
     </article>
