5e6592bb6c30bf18f92482e06153af03203d3b1c
Edgar Méndez <mdzredgar@gmail.com>
2023-02-19 21:05:25 -0600

    Finalizada paginación

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -4,12 +4,12 @@ import { useSelector } from 'react-redux'
 import PokemonCard from '../components/pokedex/PokemonCard'
 
 const Pokedex = () => {
-  //!Todo Corregir errores
   const [pokemons, setPokemons] = useState([])
   const [pokemonsFilter, setPokemonsFilter] = useState([])
   const [types, setTypes] = useState([])
   const [selectType, setSelectType] = useState("")
   const [pokemonName, setPokemonName] = useState("")
+  const [currentPage, setCurrentPage] = useState(1)
 
   const nameTrainer = useSelector(store => store.nameTrainer)
 
@@ -20,12 +20,59 @@ const Pokedex = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     setPokemonName(e.target.pokemonName.value)
-    
+  }
+
+  const paginationLogic = () => {
+    //Cantidad de pokemons por pagina
+    const pokemonPerPage = 12
+
+    //Pokemons que se van a mostrar en la pagina actual
+    const sliceStart = (currentPage -1 )* pokemonPerPage
+    const sliceEnd = sliceStart + pokemonPerPage
+    const pokemonsInPage = pokemonsFilter.slice(sliceStart, sliceEnd)
+
+    //Ultima pagina
+    const lastPage = Math.ceil(pokemonsFilter.length / pokemonPerPage) || 1
+
+    //Bloque actual
+    const pagesPerBlock = 5
+    const actualBlock = Math.ceil(currentPage / pagesPerBlock)
+
+    //Pagina sque se van a mostar en el bloque actual
+    const pagesInBlock = []
+    const minPage = (actualBlock * pagesPerBlock - pagesPerBlock) +1
+    const maxPage = actualBlock * pagesPerBlock
+    for(let i = minPage; i <= maxPage; i++){
+      if(i <= lastPage){
+        pagesInBlock.push(i)
+      }
+    }
+
+    return {pagesInBlock, lastPage, pokemonsInPage}
+  }
+
+  const {pagesInBlock, lastPage, pokemonsInPage} = paginationLogic()
+
+  const handleNextPage = () => {
+    const newPage = currentPage +1
+    if(newPage > lastPage){
+      setCurrentPage(1)
+    } else{
+      setCurrentPage(newPage)
+    }
+  }
+
+  const handlePreviusPage = () => {
+    const newPage = currentPage - 1
+    if(newPage < 1){
+      setCurrentPage(lastPage)
+    }else{
+      setCurrentPage(newPage)
+    }
   }
 
   useEffect(() => {
     const URL = `https://pokeapi.co/api/v2/${selectType ? `type/${selectType}/` : "pokemon/?limit=20" }`
-    console.log(URL)
     axios.get(URL)
     .then((res) => {
       if(selectType){
@@ -50,11 +97,14 @@ const Pokedex = () => {
 
   useEffect(() => {
     const URL = "https://pokeapi.co/api/v2/type/"
-    axios
-    .get(URL)
+    axios.get(URL)
     .then((res) => setTypes(res.data.results))
     .catch((err) => console.log(err))
   }, [])
+
+  useEffect(() => {
+    setCurrentPage(1)
+  }, [pokemons])
 
   return (
     <main>
@@ -73,9 +123,19 @@ const Pokedex = () => {
       </form>
       <section>
         {
-          pokemonsFilter.map(pokemon => <PokemonCard key={pokemon.url} pokemonUrl={pokemon.url}/>)
-          //{pokemonsFilter.map}
+          pokemonsInPage.map(pokemon => <PokemonCard key={pokemon.url} pokemonUrl={pokemon.url}/>)
         }
+      </section>
+      <section>
+        <ul>
+          <li onClick={handlePreviusPage} >{"<<"}</li>
+          <li onClick={() => setCurrentPage(1)} >...</li>
+          {
+            pagesInBlock.map(page => <li onClick={() => setCurrentPage(page)} key={page}>{page}</li>)
+          }
+          <li onClick={() => setCurrentPage(lastPage)}>...</li>
+          <li onClick={handleNextPage} >{">>"}</li>
+        </ul>
       </section>
     </main>
   )
