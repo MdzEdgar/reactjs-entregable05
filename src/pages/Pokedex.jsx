674fe4e2dda1580664ef2075543b63b5777c0c35
Edgar Méndez <mdzredgar@gmail.com>
2023-02-21 00:32:07 -0600

    Separación de paginación

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import PokemonCard from '../components/pokedex/PokemonCard'
+import { paginationLogic } from '../utils/pagination'
 
 const Pokedex = () => {
   const [pokemons, setPokemons] = useState([])
@@ -22,36 +23,7 @@ const Pokedex = () => {
     setPokemonName(e.target.pokemonName.value)
   }
 
-  const paginationLogic = () => {
-    //Cantidad de pokemons por pagina
-    const pokemonPerPage = 12
-
-    //Pokemons que se van a mostrar en la pagina actual
-    const sliceStart = (currentPage -1 )* pokemonPerPage
-    const sliceEnd = sliceStart + pokemonPerPage
-    const pokemonsInPage = pokemonsFilter.slice(sliceStart, sliceEnd)
-
-    //Ultima pagina
-    const lastPage = Math.ceil(pokemonsFilter.length / pokemonPerPage) || 1
-
-    //Bloque actual
-    const pagesPerBlock = 5
-    const actualBlock = Math.ceil(currentPage / pagesPerBlock)
-
-    //Pagina sque se van a mostar en el bloque actual
-    const pagesInBlock = []
-    const minPage = (actualBlock * pagesPerBlock - pagesPerBlock) +1
-    const maxPage = actualBlock * pagesPerBlock
-    for(let i = minPage; i <= maxPage; i++){
-      if(i <= lastPage){
-        pagesInBlock.push(i)
-      }
-    }
-
-    return {pagesInBlock, lastPage, pokemonsInPage}
-  }
-
-  const {pagesInBlock, lastPage, pokemonsInPage} = paginationLogic()
+  const {pagesInBlock, lastPage, pokemonsInPage} = paginationLogic(pokemonsFilter, currentPage)
 
   const handleNextPage = () => {
     const newPage = currentPage +1
