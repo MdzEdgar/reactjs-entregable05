fb3a42991daa6924228a22e88103e64c65170261
Edgar Méndez <mdzredgar@gmail.com>
2023-02-22 00:00:29 -0600

    Agregado el CSS en el form

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import PokemonCard from '../components/pokedex/PokemonCard'
 import usePokedex from '../hooks/usePokedex'
+import "./styles/Pokedex.css"
 
 const Pokedex = () => {
   const nameTrainer = useSelector(store => store.nameTrainer)
@@ -19,13 +20,13 @@ const Pokedex = () => {
 
   return (
     <main className='Pokedex'>
-      <p><span>Welcome {nameTrainer}</span>, here you can find information about of your favorite pokemon</p>
-      <form onSubmit={handleSubmit}>
+      <p className='Pokedex__welcome'><span>Welcome {nameTrainer}</span>, here you can find information about of your favorite pokemon</p>
+      <form className='Pokedex__form' onSubmit={handleSubmit}>
         <div>
-          <input type="text" id="pokemonName" placeholder='search your pokemon' />
-          <button>Search</button>
+          <input className='Pokedex__form-input' type="text" id="pokemonName" placeholder='search your pokemon' />
+          <button className='Pokedex__form-btn'>Search</button>
         </div>
-        <select onChange={handleChangeSelect}>
+        <select className='Pokedex__form-select' onChange={handleChangeSelect}>
           <option value="">All</option>
           {
             types.map(type => <option key={type.url}>{type.name}</option>)
@@ -37,7 +38,7 @@ const Pokedex = () => {
           pokemonsInPage.map(pokemon => <PokemonCard key={pokemon.url} pokemonUrl={pokemon.url}/>)
         }
       </section>
-      <section>
+      <section className='Pokedex__pagination'>
         <ul>
           <li onClick={handlePreviusPage} >{"<<"}</li>
           <li onClick={() => setCurrentPage(1)} >...</li>
