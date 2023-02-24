d13353cb82a18916961b4ee7f3acc27542e65d76
Edgar Méndez <mdzredgar@gmail.com>
2023-02-23 18:31:18 -0600

    Agregado header en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,19 +14,24 @@ const Home = () => {
   }
 
   return (
-    <main className='Home'>
-      <section className='Home__section'>
-        <div className='Home__img shine'>
-          <img src="/images/pokedex.png" alt="" />
-        </div>
-        <h2 className='Home__welcome'>Hello trainer!</h2>
-        <p className='Home__instruction'>Give me your name to start!</p>
-        <form className='Home__form' onSubmit={handleSubmit}>
-          <input className='Home__form-input' id='nameTrainer' type="text" placeholder='your name...' required />
-          <button className='Home__form-btn' ><span className='blink_me'>Press Start</span></button>
-        </form>
-      </section>
-    </main>
+    <>
+      <header className='header-home'>
+        <img src="/images/pokemon.png" alt="" />
+      </header>
+      <main className='Home'>
+        <section className='Home__section'>
+          <div className='Home__img shine'>
+            <img src="/images/pokedex.png" alt="" />
+          </div>
+          <h2 className='Home__welcome'>Hello trainer!</h2>
+          <p className='Home__instruction'>Give me your name to start!</p>
+          <form className='Home__form' onSubmit={handleSubmit}>
+            <input className='Home__form-input' id='nameTrainer' type="text" placeholder='your name...' required />
+            <button className='Home__form-btn' ><span className='blink_me'>Press Start</span></button>
+          </form>
+        </section>
+      </main>
+    </>
   )
 }
 
